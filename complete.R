d3534d6997f130b2e060c99957e1f03e98d0faeb
preethical <preethi@deobald.ca>
2019-06-22 05:38:28 +0530

    nothing useful

--- complete.R
+++ complete.R
@@ -11,12 +11,12 @@ complete <- function(directory, id = 1:332) {
     # 1. Read in the CSV
     filenumber <- str_pad(i, width=3, side="left", pad="0")
     filename <- paste0(path, filenumber, ".csv")
-    casefile <-read.csv(file=filename, header=TRUE)
+    casefile <- read.csv(file=filename, header=TRUE)
     casefile1 <- na.omit (casefile)
     nobs <- nrow(casefile1)
    # nobs <- sum (complete.cases(read.csv(file=filename, header=TRUE)))
     # 2. Calculate complete cases in file 
-    # 3. Compile data frame (matrix)
+    # 3. Compile data frame (matrix)set.seed(42)
     ## Return a data frame of the form:
     ## id nobs
     ## 1  117
