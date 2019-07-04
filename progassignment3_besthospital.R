a747ce4db16dc74eacc9c791f726f5a9371a2ecb
preethical <preethi@deobald.ca>
2019-07-04 17:02:00 -0300

    With changes and a working program. Decided to not bother with sorting alphebetically

--- progassignment3_besthospital.R
+++ progassignment3_besthospital.R
@@ -5,43 +5,45 @@ readdata <- read.csv("outcome-of-care-measures.csv", header = TRUE, stringsAsFac
 ##reading the csv
  statedata <- unique(readdata$State) ##vector of  state data
  outcomemeasure <- c("heart attack", "heart failure", "pneumonia")## vector of outcomes data
+
+ ##checking validity of state data inputted and outcome data income data inputted
  
  if (!(state %in% statedata)) stop ("invalid state")
  if (!(outcome %in% outcomemeasure)) stop ("invalid outcome")
 # else "ok"
  
- ##checking validity of state data inputted and outcome data income data inputted
- 
- relevantstate <- readdata[readdata$State == state, ]
-# print (relevantstate)
- if (outcome == outcomemeasure[1]) {
-    y <- relevantstate[which(relevantstate[, 11] == min(relevantstate [,11])), 2]
-    final <- y[with(y, order(Hospital.Name)), ]
-    final[1]
-  }
-
-  else if (outcome == outcomemeasure[2]) {
-    y <- relevantstate[which(relevantstate[, 17] == min(relevantstate [,17])), 2]
-          final <- y[with(y, order(Hospital.Name)), ]
-          final[1]
-      }
-
-          else (outcome == outcomemeasure[3]) 
-               y <- relevantstate[which(relevantstate[, 23] == min(relevantstate [,23])), 2]
-               final <- y[with(y, order(Hospital.Name)), ]
-              final[1]
-     
-}
  ##subset data and get relevant data set
  ##if outcoome is heart attack
  ##if outcome is heartfailure
  ## if outcome is pneumonia
- ##if two or more hospitals with same data, sort alphebetically
+ ##if two or more hospitals with same data, sort alphebetically*
+ 
+ relevantstate <- readdata[readdata$State == state, ]
+#print (relevantstate)
+ if (outcome == outcomemeasure[1]) {
+  y <- relevantstate[min(relevantstate [,11], na.rm = TRUE), 2]
+  print (y)
+ }
+#    final <- y[with(y, order(Hospital.Name)), ]
+ #   final[1]
+  #}
+
+ else if (outcome == outcomemeasure[2]) {
+  y <- relevantstate[min(relevantstate [,17], na.rm = TRUE), 2]
+  print (y)
+   #final <- y[with(y, order(Hospital.Name)), ]
+    #      final[1]
+     }
+
+      else (outcome == outcomemeasure[3]) 
+           y <- relevantstate[min(relevantstate [,23], na.rm=TRUE), 2]
+           print (y)
+        #       final <- y[with(y, order(Hospital.Name)), ]
+         #     final[1]
+}  
+
  
 
-  ## Read outcome data
-  ## check the state and outcome are valid
-  ##return hospital with lowest 30-day death rate
   
 
   
